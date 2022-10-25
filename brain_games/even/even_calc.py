e4e126df1e9ea4fc610cb55aa402a7d83cfbd4f4
Евгений Чайка <chay67ka26@gmail.com>
2022-10-26 01:29:27 +0800

    update even_calc.py and even_logic.py

--- brain_games/even/even_calc.py
+++ brain_games/even/even_calc.py
@@ -1,9 +1,6 @@
 from random import randint
 
 
-random_number = randint(1, 20)
-
-
 def is_even(random_number):
     if random_number % 2 == 0:
         return 'yes'
--- brain_games/even/even_logic.py
+++ brain_games/even/even_logic.py
@@ -1,12 +1,14 @@
 import prompt
+from random import randint
 from brain_games.even.even_calc import is_even
-from brain_games.even.even_calc import random_number
+# from brain_games.even.even_calc import random_number
 
 
 def even_game_logic():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     attempt = 0
     while attempt < 3:
+        random_number = randint(1, 20)
         print(f'Question: {random_number}')
         your_answer = prompt.string('Your answer: ')
         if your_answer == is_even(random_number):
