678966571ecc7a28ad064eea24188fc22a63feb5
Евгений Чайка <chay67ka26@gmail.com>
2022-11-09 11:37:46 +0800

    add brain-prime game, add brain-prime command

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,30 @@
+from random import randint
+from math import sqrt, ceil
+from brain_games.logic.game_logic import logic
+
+rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+answer_list = [0]
+numbers_list = list(range(1, 20))
+
+
+def task():
+    random_number_from_list = randint(0, len(numbers_list) - 1)
+    number = numbers_list[random_number_from_list]
+    devisor = 2
+    if number == 1:
+        answer_list[0] = 'no'
+        numbers_list.pop(random_number_from_list)
+        return number
+    while number % devisor != 0:
+        devisor += 1
+    if devisor == number:
+        numbers_list.pop(random_number_from_list)
+        answer_list[0] = 'yes'
+        return number
+    numbers_list.pop(random_number_from_list)
+    answer_list[0] = 'no'
+    return number
+
+
+def game():
+    logic(rules, task, answer_list)
