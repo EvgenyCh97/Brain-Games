21208addd81740265eb8019952f25ee6c767a2a9
Евгений Чайка <chay67ka26@gmail.com>
2022-11-10 10:11:50 +0800

    progression_game fix lint

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -5,18 +5,19 @@ rules = 'What number is missing in the progression?'
 answer_list = ['']
 
 
-def get_progression():
-    step_of_progression = randint(2, 4)
-    len_of_progression = randint(5, 10)
-    elements_count = len_of_progression * step_of_progression
-    start_of_progression = randint(0, 100 - elements_count)
-    stop_of_progression = start_of_progression + elements_count
-    result_list = list(map(str, range(1, 101)[start_of_progression:stop_of_progression:step_of_progression]))
+def get_progression_list():
+    progression_step = randint(2, 4)
+    progression_len = randint(5, 10)
+    elements_count = progression_len * progression_step
+    progression_start = randint(0, 100 - elements_count)
+    progression_stop = progression_start + elements_count
+    str_list = list(map(str, range(1, 101)))
+    result_list = str_list[progression_start:progression_stop:progression_step]
     return result_list
 
 
 def task():
-    progression_list = get_progression()
+    progression_list = get_progression_list()
     random_number_from_list = randint(0, len(progression_list) - 1)
     answer_list[0] = progression_list[random_number_from_list]
     progression_list[random_number_from_list] = '..'
