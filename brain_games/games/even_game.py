5e86eeed4880e6caa097c306836e399f816a8d64
Евгений Чайка <chay67ka26@gmail.com>
2022-11-15 09:45:37 +0800

    update brain-even game

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -2,16 +2,12 @@ from random import randint
 
 ANSWER_LIST = ['']
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
-NUMBERS_LIST = list(range(1, 21))
 
 
 def task():
-    random_number_index = randint(0, len(NUMBERS_LIST) - 1)
-    random_number = NUMBERS_LIST[random_number_index]
+    random_number = randint(1, 20)
     if random_number % 2 == 0:
         ANSWER_LIST[0] = 'yes'
-        NUMBERS_LIST.pop(random_number_index)
     else:
         ANSWER_LIST[0] = 'no'
-        NUMBERS_LIST.pop(random_number_index)
     return random_number
