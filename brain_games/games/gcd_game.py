e9e31a518267f58a2874c80e2a2fc3836abffe1e
Евгений Чайка <chay67ka26@gmail.com>
2022-11-15 10:38:12 +0800

    fix magick numbers in gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -5,13 +5,13 @@ ANSWER_LIST = ['']
 
 
 def task():
-    first_number = randint(1, 10)
-    second_number = randint(1, 10)
-    question_list = [first_number, second_number]
-    while first_number != 0 and second_number != 0:
-        if first_number > second_number:
-            first_number = first_number % second_number
+    first_given_number = randint(1, 10)
+    second_given_number = randint(1, 10)
+    question_list = [first_given_number, second_given_number]
+    while first_given_number != 0 and second_given_number != 0:
+        if first_given_number > second_given_number:
+            first_given_number = first_given_number % second_given_number
         else:
-            second_number = second_number % first_number
-    ANSWER_LIST[0] = str(first_number + second_number)
+            second_given_number = second_given_number % first_given_number
+    ANSWER_LIST[0] = str(first_given_number + second_given_number)
     return f'{question_list[0]} {question_list[1]}'
