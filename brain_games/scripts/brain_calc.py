7b93dfe8bb45f466138d3cb55d455a75e5d9057c
Евгений Чайка <chay67ka26@gmail.com>
2022-11-15 08:25:09 +0800

    update brain-calc script

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-from brain_games.games.calc_game import rules
-from brain_games.games.calc_game import task
-from brain_games.games.calc_game import answer_list
+from brain_games.games.calc_game import rules, task, answer_list
 from brain_games.logic.game_logic import logic
 
 
