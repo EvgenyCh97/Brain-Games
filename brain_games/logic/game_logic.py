d844175d58583f4e3199789400982451e3cfc548
EvgenyCh97 <114888168+EvgenyCh97@users.noreply.github.com>
2022-11-15 11:32:42 +0800

    refactoring game_logic.py, fix magic numbers

--- brain_games/logic/game_logic.py
+++ brain_games/logic/game_logic.py
@@ -1,18 +1,19 @@
 import prompt
 
+NUMBER_OF_ROUNDS = 3
+
 
 def logic(RULES, task, ANSWER_LIST):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(RULES)
-    attempt = 0
-    while attempt < 3:
+    while NUMBER_OF_ROUNDS != 0:
         print(f'Question: {task()}')
         your_answer = prompt.string('Your answer: ')
         if your_answer == ANSWER_LIST[0]:
             print('Correct!')
-            attempt += 1
+            attempt -= 1
         else:
             print(f'"{your_answer}" is wrong answer ;(.'
                   f' Correct answer was "{ANSWER_LIST[0]}".'
