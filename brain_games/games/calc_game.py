2ea2661ebb1a049df998a7f004792dd8b4d7ba5b
EvgenyCh97 <114888168+EvgenyCh97@users.noreply.github.com>
2022-11-15 10:51:59 +0800

    Update calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -7,17 +7,17 @@ shuffle(OPERATOR_QUEUE)
 
 
 def task():
-    first_number = randint(1, 20)
-    second_number = randint(1, 20)
+    first_given_number = randint(1, 20)
+    second_given_number = randint(1, 20)
     if OPERATOR_QUEUE[0] == 1:
-        ANSWER_LIST[0] = str(first_number + second_number)
+        ANSWER_LIST[0] = str(first_given_number + second_given_number)
         OPERATOR_QUEUE.pop(0)
-        return f'{first_number} + {second_number}'
+        return f'{first_given_number} + {second_given_number}'
     elif OPERATOR_QUEUE[0] == 2:
-        ANSWER_LIST[0] = str(first_number - second_number)
+        ANSWER_LIST[0] = str(first_given_number - second_given_number)
         OPERATOR_QUEUE.pop(0)
-        return f'{first_number} - {second_number}'
+        return f'{first_given_number} - {second_given_number}'
     else:
-        ANSWER_LIST[0] = str(first_number * second_number)
+        ANSWER_LIST[0] = str(first_given_number * second_given_number)
         OPERATOR_QUEUE.pop(0)
-        return f'{first_number} * {second_number}'
+        return f'{first_given_number} * {second_given_number}'
