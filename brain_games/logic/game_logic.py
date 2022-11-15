1534a30f657ca7abf9c0b93bee53737bb377500f
EvgenyCh97 <114888168+EvgenyCh97@users.noreply.github.com>
2022-11-15 11:34:34 +0800

    Update game_logic.py

--- brain_games/logic/game_logic.py
+++ brain_games/logic/game_logic.py
@@ -13,7 +13,7 @@ def logic(RULES, task, ANSWER_LIST):
         your_answer = prompt.string('Your answer: ')
         if your_answer == ANSWER_LIST[0]:
             print('Correct!')
-            attempt -= 1
+            NUMBER_OF_ROUNDS -= 1
         else:
             print(f'"{your_answer}" is wrong answer ;(.'
                   f' Correct answer was "{ANSWER_LIST[0]}".'
