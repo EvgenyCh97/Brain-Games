7fde6e3dd22ddfd9df70ed2853d649b47f7dd894
Евгений Чайка <chay67ka26@gmail.com>
2022-11-21 11:13:57 +0800

    refactoring brain-even game

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,6 +1,5 @@
 from random import randint
 
-ANSWER_LIST = ['']
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 MIN_GIVEN_NUMBER = 1
 MAX_GIVEN_NUMBER = 20
@@ -9,7 +8,6 @@ MAX_GIVEN_NUMBER = 20
 def task():
     random_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
     if random_number % 2 == 0:
-        ANSWER_LIST[0] = 'yes'
+        return (random_number, 'yes')
     else:
-        ANSWER_LIST[0] = 'no'
-    return random_number
+        return (random_number, 'no')
