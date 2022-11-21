cf6a93e1a88552e859a4a166b9ae6582bffc6c73
Евгений Чайка <chay67ka26@gmail.com>
2022-11-21 11:24:22 +0800

    refactoring brain-prime game

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -1,7 +1,6 @@
 from random import randint
 
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-ANSWER_LIST = ['']
 MIN_GIVEN_NUMBER = 1
 MAX_GIVEN_NUMBER = 20
 
@@ -10,12 +9,9 @@ def task():
     random_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
     divisor = 2
     if random_number == 1:
-        ANSWER_LIST[0] = 'no'
-        return random_number
+        return (random_number, 'no')
     while random_number % divisor != 0:
         divisor += 1
     if divisor == random_number:
-        ANSWER_LIST[0] = 'yes'
-        return random_number
-    ANSWER_LIST[0] = 'no'
-    return random_number
+        return (random_number, 'yes')
+    return (random_number, 'no')
