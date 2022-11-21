05d6c866cd82cb039400ae69093de0fc0d1e7683
Евгений Чайка <chay67ka26@gmail.com>
2022-11-21 11:00:58 +0800

    refactoring game_logic.py and brain-calc game

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,9 +1,6 @@
-from random import randint, shuffle
+from random import randint
 
 RULES = 'What is the result of the expression?'
-ANSWER_LIST = ['']
-OPERATOR_QUEUE = [1, 2, 3]
-shuffle(OPERATOR_QUEUE)
 MIN_GIVEN_NUMBER = 1
 MAX_GIVEN_NUMBER = 20
 
@@ -11,15 +8,16 @@ MAX_GIVEN_NUMBER = 20
 def task():
     first_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
     second_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
-    if OPERATOR_QUEUE[0] == 1:
-        ANSWER_LIST[0] = str(first_number + second_number)
-        OPERATOR_QUEUE.pop(0)
-        return f'{first_number} + {second_number}'
-    elif OPERATOR_QUEUE[0] == 2:
-        ANSWER_LIST[0] = str(first_number - second_number)
-        OPERATOR_QUEUE.pop(0)
-        return f'{first_number} - {second_number}'
+    if first_number + second_number < MAX_GIVEN_NUMBER:
+        question = f'{first_number} * {second_number}'
+        correct_answer = str(first_number * second_number)
+        return (question, correct_answer)
     else:
-        ANSWER_LIST[0] = str(first_number * second_number)
-        OPERATOR_QUEUE.pop(0)
-        return f'{first_number} * {second_number}'
+        if first_number >= second_number:
+            question = f'{first_number} - {second_number}'
+            correct_answer = str(first_number - second_number)
+            return (question, correct_answer)
+        elif second_number > first_number:
+            question = f'{first_number} + {second_number}'
+            correct_answer = str(first_number + second_number)
+            return (question, correct_answer)
--- brain_games/logic/game_logic.py
+++ brain_games/logic/game_logic.py
@@ -2,21 +2,22 @@ import prompt
 NUMBER_OF_ROUNDS = 3
 
 
-def logic(RULES, task, ANSWER_LIST):
+def logic(RULES, task):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(RULES)
     round = NUMBER_OF_ROUNDS
     while round != 0:
-        print(f'Question: {task()}')
+        (question, correct_answer) = task()
+        print(f'Question: {question}')
         your_answer = prompt.string('Your answer: ')
-        if your_answer == ANSWER_LIST[0]:
+        if your_answer == correct_answer:
             print('Correct!')
             round -= 1
         else:
             print(f'"{your_answer}" is wrong answer ;(.'
-                  f' Correct answer was "{ANSWER_LIST[0]}".'
+                  f' Correct answer was "{correct_answer}".'
                   f'\nLet\'s try again, {name}!')
             return
     print(f'Congratulations, {name}!')
