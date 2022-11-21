ca09b69cad847c143dfa169831011f285abf43a7
Евгений Чайка <chay67ka26@gmail.com>
2022-11-21 13:04:49 +0800

    refactoring game_logic

--- brain_games/logic/game_logic.py
+++ brain_games/logic/game_logic.py
@@ -2,14 +2,14 @@ import prompt
 NUMBER_OF_ROUNDS = 3
 
 
-def logic(RULES, task):
+def logic(game_module):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(RULES)
+    print(game_module.RULES)
     round = NUMBER_OF_ROUNDS
     while round != 0:
-        (question, correct_answer) = task()
+        (question, correct_answer) = game_module.task()
         print(f'Question: {question}')
         your_answer = prompt.string('Your answer: ')
         if your_answer == correct_answer:
