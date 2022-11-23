d6a52448987558358a13e1c107f7fdaded2b3933
Евгений Чайка <chay67ka26@gmail.com>
2022-11-23 17:39:46 +0800

    refactoring calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -3,22 +3,21 @@ from random import randint, choice
 RULES = 'What is the result of the expression?'
 MIN_GIVEN_NUMBER = 1
 MAX_GIVEN_NUMBER = 20
-OPERATORS = ('+', '-', '*')
 
 
 def get_task_and_answer():
     first_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
     second_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
-    operator = choice(OPERATORS)
-    if operator == '*':
-        question = f'{first_number} * {second_number}'
-        correct_answer = str(first_number * second_number)
-        return (question, correct_answer)
-    if operator == '-':
-        question = f'{first_number} - {second_number}'
-        correct_answer = str(first_number - second_number)
-        return (question, correct_answer)
+    operator = choice(['+', '-', '*'])
     if operator == '+':
         question = f'{first_number} + {second_number}'
         correct_answer = str(first_number + second_number)
+        return (question, correct_answer)
+    elif operator == '-':
+        question = f'{first_number} - {second_number}'
+        correct_answer = str(first_number - second_number)
+        return (question, correct_answer)
+    else:
+        question = f'{first_number} * {second_number}'
+        correct_answer = str(first_number * second_number)
         return (question, correct_answer)
