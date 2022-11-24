e77e19678339a0f2edafc864759a2c033621a497
Евгений Чайка <chay67ka26@gmail.com>
2022-11-24 23:48:15 +0800

    rename variables in calc_game

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,13 +1,13 @@
 from random import randint, choice
 
 RULES = 'What is the result of the expression?'
-MIN_GIVEN_NUMBER = 1
-MAX_GIVEN_NUMBER = 20
+MIN_NUMBER = 1
+MAX_NUMBER = 20
 
 
 def get_task_and_answer():
-    first_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
-    second_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
+    first_number = randint(MIN_NUMBER, MAX_NUMBER)
+    second_number = randint(MIN_NUMBER, MAX_NUMBER)
     operator = choice(['+', '-', '*'])
     if operator == '+':
         question = f'{first_number} + {second_number}'
