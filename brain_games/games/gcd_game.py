93a0bd05c24ee965c28a441a0c852a7caa1b833f
Евгений Чайка <chay67ka26@gmail.com>
2022-11-25 00:18:22 +0800

    rafactoring gcd_game

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -1,4 +1,5 @@
 from random import randint
+from math import gcd
 
 RULES = 'Find the greatest common divisor of given numbers.'
 MIN_GIVEN_NUMBER = 1
@@ -9,10 +10,5 @@ def get_task_and_answer():
     first_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
     second_number = randint(MIN_GIVEN_NUMBER, MAX_GIVEN_NUMBER)
     question = f'{first_number} {second_number}'
-    while first_number != 0 and second_number != 0:
-        if first_number > second_number:
-            first_number = first_number % second_number
-        else:
-            second_number = second_number % first_number
-    correct_answer = str(first_number + second_number)
+    correct_answer = str(gcd(first_number, second_number))
     return (question, correct_answer)
