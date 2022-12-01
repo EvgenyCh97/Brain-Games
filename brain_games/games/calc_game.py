ab738602dfb63b2cc58b18c6a308480cc50acde4
Евгений Чайка <chay67ka26@gmail.com>
2022-12-01 14:52:57 +0800

    create calculate_result() func in calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,23 +1,26 @@
 from random import randint, choice
+from operator import add, mul, sub
 
 RULES = 'What is the result of the expression?'
 MIN_NUMBER = 1
 MAX_NUMBER = 20
 
 
+def calculate_result(first_number, second_number, operator):
+    result = str(operator(first_number, second_number))
+    if operator == add:
+        question = f'{first_number} + {second_number}'
+        return (question, result)
+    elif operator == sub:
+        question = f'{first_number} - {second_number}'
+        return (question, result)
+    else:
+        question = f'{first_number} * {second_number}'
+        return (question, result)
+
+
 def get_task_and_answer():
     first_number = randint(MIN_NUMBER, MAX_NUMBER)
     second_number = randint(MIN_NUMBER, MAX_NUMBER)
-    operator = choice(['+', '-', '*'])
-    if operator == '+':
-        question = f'{first_number} + {second_number}'
-        correct_answer = str(first_number + second_number)
-        return (question, correct_answer)
-    elif operator == '-':
-        question = f'{first_number} - {second_number}'
-        correct_answer = str(first_number - second_number)
-        return (question, correct_answer)
-    else:
-        question = f'{first_number} * {second_number}'
-        correct_answer = str(first_number * second_number)
-        return (question, correct_answer)
+    operator = choice((add, mul, sub))
+    return calculate_result(first_number, second_number, operator)
