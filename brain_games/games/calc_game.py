3e1150be63b786a04dcb8d2e3b3dd011b6b940a1
Евгений Чайка <chay67ka26@gmail.com>
2022-12-14 14:01:11 +0800

    fix signature of calculate_result func in calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -7,14 +7,14 @@ MAX_NUMBER = 20
 
 
 def calculate_result(first_number, second_number, operator):
-    return str(operator(first_number, second_number))
+    return operator(first_number, second_number)
 
 
 def get_task_and_answer():
     first_number = randint(MIN_NUMBER, MAX_NUMBER)
     second_number = randint(MIN_NUMBER, MAX_NUMBER)
     operator = choice((add, mul, sub))
-    result = calculate_result(first_number, second_number, operator)
+    result = str(calculate_result(first_number, second_number, operator))
     if operator == add:
         question = f'{first_number} + {second_number}'
         return (question, result)
